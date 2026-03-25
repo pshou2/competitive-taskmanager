6f636e64df331b2f3e5600dfba956536eb5ab8ae
Phil Shou <philshou@gmail.com>
2026-03-25 17:12:25 -0400

    auto generate updated and createdat dates, make emails unique

--- src/models/User.ts
+++ src/models/User.ts
@@ -4,13 +4,17 @@ import { Schema, model} from 'mongoose';
 interface IUser {
     name: string;
     email: string;
+    createdAt: Date;
+    updatedAt: Date;
     //tasks: Task[];
 }
 
 ///create schema
 const userSchema = new Schema<IUser>({
     name: { type: String, required: true },
-    email: { type: String, required: true}
+    email: { type: String, required: true, unique: true }
+}, {
+    timestamps: true
 }); 
 
 ///export model
