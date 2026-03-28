0e07dda29e51d69cd2d7e13b2be6eefe227b0e57
Phil Shou <philshou@gmail.com>
2026-03-27 23:35:30 -0400

    working on controller

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,29 @@
 //import
 import { User, IUser } from '../models/User';
 
+// export class UserService {    
+//     async getAllUsers(): Promise<IUser[]> {
+//         try {
+//             const users = await User.find({});
+//             return users;
+//         } catch (error) {
+//             //what is error when we catch error?
+//             throw new Error("Failed to query database for getAllUsers()", { cause: error });
+//         }
+//     }
+
+//     async getUserById(id: string): Promise<IUser | null> {
+//         try {
+//             const user = await User.findById(id);
+//             return user;
+//         } catch (error){
+//             throw new Error (`Failed to query database for getUserById() : ${id}`, { cause: error });
+//         }
+//     }
+// }
+
+//how do i test out just this file? What should I return? I want to return a list of users, of that interface type probably
+
 export async function getAllUsers(): Promise<IUser[]> {
     try {
         const users = await User.find({});
@@ -20,3 +43,5 @@ export async function getUserById(id: string): Promise<IUser | null> {
         throw new Error (`Failed to query database for getUserById() : ${id}`, { cause: error });
     }
 }
+
+
