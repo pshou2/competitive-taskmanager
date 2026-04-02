c24c109b66640630cbbd2ccd308330bfce54ff0d
Phil Shou <philshou@gmail.com>
2026-04-01 23:02:06 -0400

    finish userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -9,8 +9,8 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
         //404?
         //500?
     try {
-        //if userService.getAllUsers returns Promise<IUser[]>, wtf do I do with that?
-        
+        //if userService.getAllUsers returns Promise<IUser[]>, what do I do with that?
+
         const users = await userService.getAllUsers();
 
         if (!users) {
@@ -20,6 +20,21 @@ export const getAllUsers = async (req: Request, res: Response, next: NextFunctio
         res.status(200).json(users);
     } catch (error) {
         //what is next function? how and when do we use that? what should prompt my brain to think 'Oh i should use a next function'
-        next(Error);
+        next(error);
+    }
+}
+
+export const getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const id = Array.isArray(req.params.id) ? req.params.id[0] : req.params.id;
+        const user = await userService.getUserById(id);
+        
+        if  (!user) {
+            return res.status(404).json({ message: `User with id ${id} not found`});
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        next(error);
     }
 }
