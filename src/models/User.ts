6de6c722076c06d181316ac1973c60e5e48639ed
Phil Shou <philshou@gmail.com>
2026-04-06 15:16:27 -0400

    revised user model

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,21 +1,38 @@
-import { Schema, model} from 'mongoose';
+import { Schema, model, Types } from 'mongoose';
 
 ///create interface
 export interface IUser {
-    name: string;
+    googleId: string;
+    username: string;
     email: string;
-    createdAt: Date;
-    updatedAt: Date;
-    //tasks: Task[];
+    groups: Types.ObjectId[];
 }
 
 ///create schema
 const userSchema = new Schema<IUser>({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true }
+    googleId: {
+        type: String, 
+        required: true
+    },
+    username: { 
+        type: String, 
+        required: [ true, 'Username is required'],
+        trim: true,
+        minlength: [2, 'Username must be at least 2 characters'] 
+    },
+    email: { 
+        type: String, 
+        required: [true, 'Email is required'], 
+        unique: true,
+        lowercase: true 
+    },
+    groups: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Group'
+    }]
 }, {
     timestamps: true
 }); 
 
 ///export model
-export const User = model<IUser>('users', userSchema);
+export const User = model<IUser>('User', userSchema);
