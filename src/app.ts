e95256aad3322e94e24e8146bd2123c37f790780
Phil Shou <philshou@gmail.com>
2026-05-22 21:16:03 -0400

    tested happy paths for register and login

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
-app.use("api/auth", authRouter);
+app.use("/api/auth", authRouter);
 
 app.use("/api/users", userRouter);
 app.use("/api/tasks", taskRouter);
